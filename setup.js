ef21885b49e2d8557be28d719d1977daa21e3988
FlareonTheProgrammer <theinnovator64@gmail.com>
2020-06-23 00:54:26 -0500

    changes

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
+const flogger = require("flogger");
+const fs = require("fs");
 const inquirer = require("inquirer");
 const { cData } = require("./src/main.js");
-const fs = require("fs");
 
 let setup = [
   {
@@ -22,22 +23,51 @@ let clientInfo = [
     message: "Please enter the client secret of your Spotify application.",
   },
 ];
+let confirmOverwrite = [
+  {
+    type: "list",
+    name: "overwriteCheck",
+    message: "A json.sqlite file was found inside your project's root folder. Are you sure you want to overwrite it?",
+    choices: ["Yes", "No"],
+  }
+];
 (async function () {
   const beginSetup = await inquirer.prompt(setup);
   if (beginSetup.proceed === "Yes") {
-    console.log("Got it. Beginning setup of easySpotify...");
-    const checks = {
-      dbInProjFolder: fs.existsSync(`./json.sqlite`),
-      dbInParentOfParentFolder: fs.existsSync("../../json.sqlite"),
-      installedInProjectFolder: fs.existsSync("../../node_modules")
+    flogger.log("Beginning setup of easySpotify...");
+
+    var chkInt = 0; // Initialize the check integer
+
+    // Checks if a db file exists in easySpotify's project folder
+    if (fs.existsSync(`./json.sqlite`)) {
+      chkInt = chkInt + 1;
+    };
+
+    // Checks if a db file exists in the parent of the parent folder
+    if (fs.existsSync("../../json.sqlite")) {
+      chkInt = chkInt + 2;
+    };
+
+    // Checks if easySpotify exists inside of a node_modules folder
+    if (fs.existsSync("../../node_modules")) {
+      chkInt = chkInt + 4;
+    };
+    chkInt = (new Number(chkInt));
+    console.log(chkInt & 7);
+
+    switch (chkBin) {
+      case 0:
+        return flogger.error(
+          "This is a bit of a problem. The json.sqlite file wasn't found in easySpotify's project folder."
+        )
     };
     const resp = await inquirer.prompt(clientInfo);
     cData.set("id", resp.id);
     cData.set("secret", resp.secret);
-    console.log("Client data file created.");
+    flogger.log("Client data file created.");
     if (fs.existsSync(`./json.sqlite`) && !fs.existsSync("../../json.sqlite") && fs.existsSync("../../node_modules")) {
-      console.log(
-        "[DEBUG] \x1b[36mjson.sqlite file found in easyspotify module folder, attempting to move it...\x1b[0m"
+      flogger.log(
+        "json.sqlite file found in easyspotify module folder, attempting to move it..."
       );
       try {
         fs.renameSync("./json.sqlite", "../../json.sqlite");
